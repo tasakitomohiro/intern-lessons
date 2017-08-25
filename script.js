81590127c4a1fe21db108265fd7b7052f5a4e5e0
tasakitomohiro <tasakitomohiro32@gmail.com>
2017-08-25 11:33:23 +0900

    popup add

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#umi-image').click(function() {
+    $('#umi-modal').modal('show');
+  });
+  $('#syuri-image').click(function() {
+    $('#syuri-modal').modal('show');
+  });
+  $('#hedo-image').click(function() {
+    $('#hedo-modal').modal('show');
+  });
+  $('#kourijima-image').click(function() {
+    $('#kourijima-modal').modal('show');
+  });
+});
