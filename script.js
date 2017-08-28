53116bd203cbd9b47f9159e8a68678f9211fcb80
tasakitomohiro <tasakitomohiro32@gmail.com>
2017-08-28 14:21:40 +0900

    空白追加

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
   $('#kourijima-image').click(function() {
     $('#kourijima-modal').modal('show');
   });
+
+  var corsOveride = "https://cors-anywhere.herokuapp.com/";
+  var coreUrl = "https://api.darksky.net/forecast/";
+  var apiKey = "5d37496152fc94336f98c7caa52ab5e1";/*gitに挙げる前に要変更*/
+  var lat = 26
+  var lon = 127
+  var excluded = "exclude=Minutely,Hourly,Daily,Alerts,Flags"
+
+  //それで+を使ってわけた部分を合併する
+  var url = corsOveride + coreUrl + apiKey + '/' + lat + ',' + lon + '?' + excluded;
+
+  //そこから.getJsonではurlだけを渡せばいい
+  $.getJson(url, function (data) {
+      console.log(data);
+  });
 });
